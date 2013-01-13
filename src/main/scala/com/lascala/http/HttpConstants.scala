b2e814dce6a13392f8abc2bf95b1769bdd35f11c
Yi EungJun <semtlenori@gmail.com>
2013-01-13 22:04:47 +0900

    Add missed HttpConstants.scala.

---
+++ src/main/scala/com/lascala/http/HttpConstants.scala
@@ -0,0 +1,19 @@
+package com.lascala.http
+
+import akka.util.ByteString
+
+/**
+ * HTTP 상수 모음
+ */
+object HttpConstants {
+  val SP = ByteString(" ")
+  val HT = ByteString("\t")
+  val CRLF = ByteString("\r\n")
+  val COLON = ByteString(":")
+  val PERCENT = ByteString("%")
+  val PATH = ByteString("/")
+  val QUERY = ByteString("?")
+  val RFC1123_DATE_PATTERN = "EEE, dd MMM yyyy HH:mm:ss zzz"
+}
+
+// vim: set ts=2 sw=2 et:
