35659857816d6a7c536364e73e139e1123f65206
Eun Woo Song <eun@tapad.com>
2013-01-20 22:45:52 -0500

    Chunked encoding support

---
+++ src/main/scala/com/lascala/libs/Enumerator.scala
@@ -0,0 +1,116 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *  
+ *  Copyright 2009-2012 Typesafe Inc. <http://www.typesafe.com>
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. 
+ */
+package com.lascala.libs
+
+import akka.actor.IO
+import akka.util.ByteString
+import concurrent.{Await, Future}
+import concurrent.ExecutionContext.Implicits.global
+import java.io.InputStream
+import util.{Success, Failure}
+import concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * Chunked encoding 을 서포트 하기위해 기본적인 Enumerator(or Producer) 패턴 을 구현한것이다.
+ * future 가 각각 처리되어야 할 데이타 를 담당한다.
+ * Foreach 메소드 가 이 Enumerator 와 연결되어있는 다른 enumerator 를 연동해
+ * 각 Enumerator 의 future 를 호출해서 각각 chunk 데이타를 처리한다. 
+ *
+ */
+trait Enumerator[T] {
+  parent =>
+
+  /**
+   * 스칼라 Future 를 사용해서 인풋을 처리함.
+   * 
+   * @note Some 은 계속해서 처리할 인풋이 있다는 뜻이고 None 은 그 반대임.
+   */
+  def future: Future[Option[T]]
+
+ /**
+  * 각각 Enumerator 의 퓨처를 호출한후 리턴된 값을 주어진 콜백 function 에 
+  * 적용한다. 퓨처는 None 을 리턴하기까지 귀재 방식으로 계속해서 호출됨.
+  *
+  * @param f callbakc function 을 사용하요 퓨처가 리턴한 값을 처리함. 
+  */
+ @scala.annotation.tailrec
+  final def foreach[E](f: T => E) {
+    Await.result(future, Duration.apply(60, TimeUnit.SECONDS)) match {
+      case Some(v) => {
+        f(v)
+        foreach(f)
+      }
+      case None => if (next.isDefined) next.get.foreach(f)
+    }
+  }
+
+  def next: Option[Enumerator[T]] = None 
+
+  def andThen(other: Enumerator[T]) = new Enumerator[T] {
+    def future: Future[Option[T]] = parent.future
+    override def next = Some(other)
+  }
+}
+
+/**
+ * Enumerator companion object.
+ */
+object Enumerator {
+  
+  def fromCallback(callback: () => Option[ByteString]) = new Enumerator[ByteString] {
+    def future = Future(callback())
+  }
+
+  /**
+   * Takes InputStream as input source and returns a Enumerator that produces data.
+   * InputStream 을 받아서 Enumerator 를 리턴해주는 유틸펑션.
+   *
+   * @param in
+   * @param chunkSize
+   */
+  def fromStream(in: InputStream, chunkSize: Int = 1024 * 8) = {
+    def callback = { () =>
+      val buffer = new Array[Byte](chunkSize)
+      val chunk = in.read(buffer) match {
+        case -1 =>
+          in.close()
+          None
+        case read =>
+          val input = new Array[Byte](read)
+          System.arraycopy(buffer, 0, input, 0, read)
+          Some(ByteString(input))
+      }
+
+      chunk
+    }
+
+    fromCallback(callback)
+  }
+
+  /**
+   * Convinient function that takes a file and returns a Enumerator
+   * 
+   * 파일 오브젝트를 받아서 Enumerator 를 리턴해주는 유틸펑션.
+   * 
+   * @param file
+   * @param chunkSize
+   */
+  def fromFile(file: java.io.File, chunkSize: Int = 1024 * 8) = fromStream(new java.io.FileInputStream(file), chunkSize)
+}
--- src/main/scala/common/main.scala
+++ src/main/scala/common/main.scala
@@ -29,6 +29,8 @@ import com.lascala.http.HttpResponse._
 import com.lascala.http.HttpConstants._
 import akka.util.ByteString
 import java.io.File
+import com.lascala.libs.Enumerator
+import org.apache.tika.Tika
 
 /**
  * Sample Demo Application
@@ -43,16 +45,30 @@ class RequestHandler extends Actor {
   val docroot = "."
 
   def receive = {
-    case HttpRequest("GET", pathSegments, _, _, headers, _) =>
+    case HttpRequest("GET", List("chunked"), _, _, headers, _) => 
+      sender !  OKResponse.stream(getEnumerator)
+    case HttpRequest("GET", List("download"), _, _, headers, _) =>
+      sender !  OKResponse.stream(getEnumerator).withMimeType("text/plain")
+    case HttpRequest("GET", pathSegments, _, _, headers, _) => 
       new File(docroot, "/" + pathSegments.mkString(File.separator)) match {
         case file if file.isFile() =>
           headers.find(_.name.toLowerCase == "if-modified-since") match {
             case Some(d) if HttpResponse.httpDateFormat.parse(d.value).compareTo(new java.util.Date(file.lastModified)) != -1 => sender ! NotModifiedResponse()
             case _ => sender ! OKFileResponse(file, true)
           }
-        case _ =>
-          sender ! NotFoundError()
+        case _ => sender ! NotFoundError()
       }
     case _ => sender ! MethodNotAllowedError()
+  }
+
+  private def getEnumerator = {
+    // chunked encoding 을 사용하여 사이즈가 큰 파일을 스트리밍으로 처리하기 받기.
+    val file1 = new File(docroot + "/src/main/resource", "stream_data1.txt")
+    val file2 = new File(docroot + "/src/main/resource", "stream_data2.txt")
+    val enum1 = Enumerator.fromFile(file1)
+    val enum2 = Enumerator.fromFile(file2) 
+
+    // enume 을 연결시켜서 여러가지 input source 를 한 스트림으로 순서대로 처리하기.
+    enum1 andThen enum2  
   }
 }
